72c6e9b0fc1cc6c456c6c91074314873a9659687
liuzhihai <liumir@dingtalk.com>
2021-12-03 20:15:27 +0800

    [common]新增获取节点组件 NodeHelper.cs、可序列化字典类

---
+++ Assets/_Scripts/_Common/Editor/NodeHelperEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(NodeHelper))]
+public class NodeHelperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("序列化rd_节点"))
+        {
+            NodeHelper nodeHelper = (NodeHelper)target;
+            nodeHelper.RefreshRdNodeList();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/_Scripts/_Common/Editor/TransformPosMarkEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(TransformPosMark))]
+public class TransformPosMarkEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        TransformPosMark transformPosMark = (TransformPosMark)target;
+        transformPosMark.ChangeMarkLocalPosition(transformPosMark.transform.localPosition);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
